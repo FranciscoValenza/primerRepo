1daee7ff0f3ac932a7543bb5620a8a33e25cea05
FranciscoValenza <franciscovalenza@gmail.com>
2022-08-01 19:12:09 -0300

    arreglar ejercicio

---
+++ app.js
@@ -0,0 +1,24 @@
+let fs = require('fs');  // importo libreria para leer archivos de texto
+const { listenerCount } = require('process');
+let {listarTareas, guardarTarea, borrarTarea, filtrarTarea} = require('./funcionesDeTareas');
+
+let obj_json = fs.readFileSync('./servicios/tareas.json');  // Leo el archivo con o sin 'utf-8'
+let obj_literal = JSON.parse(obj_json);  // convierto el archivo
+
+
+let TN = {
+    codigo: 8,
+    titulo: "Testear codigo",
+    estado: "En progreso" 
+}
+
+guardarTarea(obj_literal,TN);
+
+obj_literal = borrarTarea(obj_literal,8);
+
+let tareasFiltradas = filtrarTarea(obj_literal,"En progreso")
+
+listarTareas(obj_literal);
+
+listarTareas(tareasFiltradas);
+
---
+++ funcionesDeTareas.js
@@ -0,0 +1,32 @@
+let fs = require('fs');  // importo libreria para leer archivos de texto
+
+function listarTareas(estructuraTareas){
+    estructuraTareas.forEach(function(dato){
+        console.log("Tarea: " + dato.titulo);
+    });
+}
+
+function guardarTarea(estructuraTareas,nuevaTarea){
+    estructuraTareas.push(nuevaTarea);
+    fs.writeFileSync('./servicios/tareas.json',JSON.stringify(estructuraTareas,null," "));
+}
+
+function borrarTarea(estructuraTareas,codigoBusqueda){
+    let nuevaEstructuraTareas = estructuraTareas.filter(function(elemento){
+        return elemento.codigo!=codigoBusqueda;
+    });
+
+    fs.writeFileSync('./servicios/tareas.json',JSON.stringify(nuevaEstructuraTareas,null," "));
+
+    return nuevaEstructuraTareas;
+}
+
+function filtrarTarea(estructuraTareas,estadoFiltro){
+    let nuevaEstructuraTareas = estructuraTareas.filter(function(elemento){
+        return elemento.estado==estadoFiltro;
+    });
+
+    return nuevaEstructuraTareas;
+}
+
+module.exports = {listarTareas, guardarTarea, borrarTarea, filtrarTarea}
